e8264b1983e7e83447b480d9b990fde218ab32ad
livghit <liviu.g01@gmail.com>
2023-09-09 18:36:08 +0200

    yae

--- cmd/main.go
+++ cmd/main.go
@@ -49,6 +49,7 @@ type itemDelegate struct{}
 func (d itemDelegate) Height() int                               { return 1 }
 func (d itemDelegate) Spacing() int                              { return 0 }
 func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+
 func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
 	i, ok := listItem.(Item)
 	if !ok {
